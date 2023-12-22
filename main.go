429c4daa4e19b704b5641a95d123f5ccf68bd870
DEARn6sx <nantawats.study@gmail.com>
2023-12-22 12:46:36 +0700

    describe package and fnc

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
-package main
+package main //คำสั่งและpackage จะถูกทำงานในตอนเริ่มต้นในตอนที่ run program
 
 import "fmt" //package จัดการ input output (เกี่ยวกับการแสดงผล)
 
 //func main = fnc ที่ go มองอันดับแรก
 func main() {
-	fmt.Println("Hello Go Programming")
+	fmt.Println("Hello Go Programming") //แสดงผลข้อความและขึ้นบรรทัดใหม่
 }
