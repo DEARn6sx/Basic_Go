a42366771532ef27e1b9a91b85ae12793c709268
DEARn6sx <nantawats.study@gmail.com>
2023-12-22 15:12:53 +0700

    Scanf

--- main.go
+++ main.go
@@ -2,42 +2,10 @@ package main //คำสั่งและpackage จะถูกทำงาน
 
 import "fmt" //package จัดการ input output (เกี่ยวกับการแสดงผล)
 
-//func main = fnc ที่ go มองอันดับแรก
+// scanf
 func main() {
-	fmt.Println("Hello Go Programming") //แสดงผลข้อความและขึ้นบรรทัดใหม่
-
-	//นิยามตัวแปร
-	var name string = "DEAR"
-	lastname := "ZAZA"
-	var age int = 26
-	var score float32 = 95.8
-	var isPass bool = true
-	fmt.Println("My name is", name)
-	fmt.Println("My lastname is", lastname)
-	fmt.Println("My age is", age)
-	fmt.Println("My score is", score)
-	fmt.Println("My testing is", isPass)
-
-	//data type and value
-	address := "roi et"
-	fmt.Printf("My value address is %v\n", address)
-	fmt.Printf("My data type address is %T\n", address)
-
-	//math operator
-	// var num1 int = 10
-	// var num2 int = 3
-	var num1, num2 = 10, 3
-	fmt.Println("ผลบวก = ", num1+num2)
-	fmt.Println("ผลลบ = ", num1-num2)
-	fmt.Println("ผลคูณ = ", num1*num2)
-	fmt.Println("ผลหาร = ", num1/num2)
-	fmt.Println("ผลหานเอาเศษ = ", num1%num2)
-
-	//Operator compare
-	fmt.Println("เท่ากันหรือไม้  = ", num1 == num2)
-	fmt.Println("ไม่เท่ากันหรือไม้  = ", num1 != num2)
-	fmt.Println(num1, "มากกว่า ", num2, "=", num1 > num2)
-	fmt.Println(num1, "น้อยกว่า ", num2, "=", num1 < num2)
-	fmt.Println(num1, "มากกว่าเท่ากับ ", num2, "=", num1 >= num2)
-	fmt.Println(num1, "น้อยกว่าเท่ากับ ", num2, "=", num1 <= num2)
+	var name string
+	fmt.Print("ป้อนชื่อ = ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Hi = ", name)
 }
