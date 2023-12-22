274b1aa1fac1455d0c6659b9594f1eb42a1aa94e
DEARn6sx <nantawats.study@gmail.com>
2023-12-22 13:16:23 +0700

    Basic Variable

--- main.go
+++ main.go
@@ -5,4 +5,16 @@ import "fmt" //package จัดการ input output (เกี่ยวกั
 //func main = fnc ที่ go มองอันดับแรก
 func main() {
 	fmt.Println("Hello Go Programming") //แสดงผลข้อความและขึ้นบรรทัดใหม่
+
+	//นิยามตัวแปร
+	var name string = "DEAR"
+	lastname := "ZAZA"
+	var age int = 26
+	var score float32 = 95.8
+	var isPass bool = true
+	fmt.Println("My name is", name)
+	fmt.Println("My lastname is", lastname)
+	fmt.Println("My age is", age)
+	fmt.Println("My score is", score)
+	fmt.Println("My testing is", isPass)
 }
